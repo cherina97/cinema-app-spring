bd17dcaed8b53658a3801f63770a4d6e16f4096d
cherina97 <cherina97@gmail.com>
2021-04-12 14:09:41 +0300

    added logging interceptor, 6.	configured endpoint of Actuator

---
+++ src/main/java/com/epam/cinema/WebConfig.java
@@ -0,0 +1,15 @@
+package com.epam.cinema;
+
+import com.epam.cinema.interceptor.LoggerInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoggerInterceptor());
+    }
+}
