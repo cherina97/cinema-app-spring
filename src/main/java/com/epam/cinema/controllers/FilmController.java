406cbf091b182d6dca84be5c59841402fcf40fa0
cherina97 <cherina97@gmail.com>
2021-04-14 17:43:41 +0300

    added swagger2

--- src/main/java/com/epam/cinema/controllers/FilmController.java
+++ src/main/java/com/epam/cinema/controllers/FilmController.java
@@ -1,52 +1,45 @@
 package com.epam.cinema.controllers;
 
+import com.epam.cinema.api.FilmApi;
 import com.epam.cinema.controllers.assemblers.FilmAssembler;
 import com.epam.cinema.controllers.models.FilmModel;
 import com.epam.cinema.dtos.FilmDto;
 import com.epam.cinema.services.FilmService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@Controller
-@RequestMapping("/films")
+@RestController
 @Slf4j
-public class FilmController {
+public class FilmController implements FilmApi {
     private final FilmService filmService;
     private final FilmAssembler filmAssembler;
 
-    @GetMapping(value = "/{title}")
-    @ResponseStatus(HttpStatus.OK)
-    public FilmModel getFilm(@PathVariable String title) {
+    @Override
+    public FilmModel getFilm(String title) {
         FilmDto film = filmService.getFilm(title);
         return filmAssembler.toModel(film);
     }
 
-    @PostMapping()
-    @ResponseStatus(HttpStatus.CREATED)
-    public FilmModel createFilm(@Valid @RequestBody FilmDto filmDto) {
+    @Override
+    public FilmModel createFilm(FilmDto filmDto) {
         log.info("Create film: {}", filmDto);
         FilmDto film = filmService.createFilm(filmDto);
         return filmAssembler.toModel(film);
     }
 
-    @PutMapping(value = "/{title}")
-    @ResponseStatus(HttpStatus.OK)
-    public FilmModel updateFilm(@Valid @RequestBody FilmDto filmDto,
-                              @PathVariable String title) {
+    @Override
+    public FilmModel updateFilm(FilmDto filmDto,
+                                String title) {
         log.info("Update film: {} to film {}", title, filmDto);
         FilmDto film = filmService.updateFilm(filmDto, title);
         return filmAssembler.toModel(film);
     }
 
-    @DeleteMapping(value = "/{title}")
-    public ResponseEntity<Void> deleteFilm(@PathVariable String title) {
+    @Override
+    public ResponseEntity<Void> deleteFilm(String title) {
         log.info("Delete film by title: {}", title);
         filmService.deleteFilm(title);
         return ResponseEntity.noContent().build();
--- src/main/java/com/epam/cinema/controllers/UserController.java
+++ src/main/java/com/epam/cinema/controllers/UserController.java
@@ -1,56 +1,46 @@
 package com.epam.cinema.controllers;
 
+import com.epam.cinema.api.UserApi;
 import com.epam.cinema.controllers.assemblers.UserAssembler;
 import com.epam.cinema.controllers.models.UserModel;
 import com.epam.cinema.dtos.UserDto;
 import com.epam.cinema.services.UserService;
-import com.epam.cinema.validation.AdvanceInfo;
-import com.epam.cinema.validation.BasicInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
+import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping(value = "/users")
 @Slf4j
-@Validated({BasicInfo.class, AdvanceInfo.class})
-public class UserController {
+public class UserController implements UserApi {
 
     private final UserService userService;
     private final UserAssembler userAssembler;
 
-    @GetMapping(value = "/{email}")
-    @ResponseStatus(HttpStatus.OK)
-    public UserModel getUser(@PathVariable String email) {
+    @Override
+    public UserModel getUser(String email) {
         UserDto user = userService.getUser(email);
         return userAssembler.toModel(user);
     }
 
-    @PostMapping()
-    @ResponseStatus(HttpStatus.CREATED)
-    public UserModel createUser(@Valid @RequestBody UserDto userDto) {
+    @Override
+    public UserModel createUser(UserDto userDto) {
         log.info("Create user: {}", userDto);
         UserDto user = userService.createUser(userDto);
         return userAssembler.toModel(user);
     }
 
-    @PutMapping(value = "/{email}")
-    @ResponseStatus(HttpStatus.OK)
-    public UserModel updateUser(@Valid @RequestBody UserDto userDto,
-                              @PathVariable String email) {
+    @Override
+    public UserModel updateUser(UserDto userDto,
+                                String email) {
         log.info("Update user: {} to user {}", email, userDto);
         UserDto user = userService.updateUser(userDto, email);
         return userAssembler.toModel(user);
     }
 
-    @DeleteMapping(value = "/{email}")
-    public ResponseEntity<Void> deleteUser(@PathVariable String email) {
+    @Override
+    public ResponseEntity<Void> deleteUser(String email) {
         log.info("Delete user by email: {}", email);
         userService.deleteUser(email);
         return ResponseEntity.noContent().build();
